c96914ecfbd0ac45ca6d5bf2c29b1acd617f5595
Roopesh S <89971611+srjroopi@users.noreply.github.com>
2025-01-17 09:02:45 +0000

    sort

--- Algorithm/SortingAlgorithm.js
+++ Algorithm/SortingAlgorithm.js
@@ -30,12 +30,13 @@ console.log(bubbleSort(arr)) // [ -83, -56, 23, 24 ] ,Big-O = O(n^2)
 // [-83, -56, 23, 24] -- sorted
 
 // Insertion sort
-// Insertion sort is a simple sorting algorithm that works the way we sort playing cards in our hand
+// Insertion sort is a simple sorting algorithm that works the way we sort playing cards in our hand(right hand side)
+// Take one sorted element and compare with remaining element if the SE > USE then move USE to right side SE. 
 function insertionSort(arr1){
     for (let i = 1; i < arr1.length; i++) {
-        let key = arr[i]
+        let key = arr1[i]
         let j = i - 1
-        while (j >= 0 && key < arr[j]) {
+        while (j >= 0 && key < arr1[j]) {
             arr1[j + 1] = arr1[j] 
             j--
             }
@@ -43,5 +44,56 @@ function insertionSort(arr1){
         }
             return arr1
     }
-const arr1 = [-56, -83, 24, 23]
-console.log(insertionSort(arr1)) // [ -83, -56, 23, 24]
+const arr1 = [-6, 2, -8, 4, 3]
+console.log(insertionSort(arr1)) // [ -8 -6 2 3 4] BigO = O(n^2)
+
+// Quick Sort
+// we can take any element from the array like first, last, random.
+// [ -6, 2, -8, 4, 3] taking last ele 3
+// sort the less values left and greter values right
+// [-6 -8] [3] [2 4]
+// taking last -8 and placing -6 to right
+// taking last 4 and placing 2 to left
+// -8, -6, 3, 2, 4
+// again it continues to last 4 
+// -8 -6 2 3 4
+function quickSort(qui){
+    if(qui.length < 2){
+        return qui
+    }
+    let pivot = qui[qui.length-1]
+    let left=[]
+    let right=[]
+    for (let i=0; i<qui.length -1; i++){
+        if(pivot>qui[i]){
+            left.push(qui[i])
+        }else{right.push(qui[i])}
+    }
+    return[...quickSort(left), pivot, ...quickSort(right)]
+}
+const qui = [-6, 2, -8, 4, 3]
+console.log("quickSort",quickSort(qui)) // worst case O(n^2) avg case O(nlogn)
+
+// Merge sort
+function mergeSort(mer){
+    if(mer.length < 2){
+        return mer
+    }
+    const mid = Math.floor(mer.length /2)
+    const left = mer.slice(0, mid)
+    const right = mer.slice(mid)
+    return merge(mergeSort(left),mergeSort(right))
+}
+
+function merge(left, right){
+    const sortArr = []
+    while(left.length && right.length){
+        if (left[0] <= right[0]){
+            sortArr.push(left.shift())
+        } else{
+            sortArr.push(right.shift())
+        }
+    } return[...sortArr, ...left, ...right]
+}
+const mer = [-6, 2, -8, 4, 3]
+console.log("Merge Sort",mergeSort(mer))// O(nlogn)
